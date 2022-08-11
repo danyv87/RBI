2ece1cb5f9c38666a18090b038b2f9ccacae957c
Daniel <dany.v87@gmail.com>
2022-08-11 07:56:06 -0400

    cambio a shapefile de cursos hidricos segmentados

--- Calidaddeagua.py
+++ Calidaddeagua.py
@@ -63,7 +63,7 @@ for i in range(len(años)):
             idw=[]
 
             #read shapefile
-            shp = gpd.read_file(os.path.dirname(absolute_path) + "\\Shps\\Cursos_Otto4ZonalStats2.shp")
+            shp = gpd.read_file(os.path.dirname(absolute_path) + "\\Shps\\Cursos_Otto4ZonalStats2_intersect.shp")
             #shp = "C:\\Users\\danielal\\OneDrive - ITAIPU Binacional\\CIH\\Proyectos\\Modelacion Ecohidrologica\\Proyecto_QGIS\\Tetis_Incremental\\layers\\Varios\\Cursos_Otto4ZonalStats2.shp"
             #shp.plot()
             #read raster
